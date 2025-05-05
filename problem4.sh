2ab900340362ed891ee9a111f8a7f5e2e71258b0
kanguk123 <solokanguk@naver.com>
2025-05-05 15:23:12 +0900

    문제 4: 로그 파일 개수 출력 코드 추가

--- problem4.sh
+++ problem4.sh
@@ -1,5 +1,9 @@
 mkdir -p logs/old_logs
 touch logs/app.log
 touch logs/error.log
+mkdir -p old_logs
 touch old_logs/app_old.log
 find  -type f -name "*.log"
+
+echo "총 로그 파일 개수:"
+find . -type f -name "*.log" | wc -l
