e6451e62a9eb3b3a9005716a16a146f9009a92e2
kanguk123 <solokanguk@naver.com>
2025-05-05 14:59:30 +0900

    Add word count for data.txt

--- homework2.sh
+++ homework2.sh
@@ -3,3 +3,7 @@ echo -e "Hello Linux\nThis is a test\nHave a good day" > original.txt
 cp original.txt backup.txt
 mv original.txt data.txt
 tail -n 2 data.txt
+
+# data.txt 파일의 단어 수 출력
+word_count=$(wc -w < data.txt)
+echo "The number of words in data.txt is: $word_count"
